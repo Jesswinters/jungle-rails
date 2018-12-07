5776998194a26d2457a134edb4d0f1c358630549
Jessica Winters <jskawinters@gmail.com>
2018-12-07 00:04:03 +0000

    Added feature: order email sends after order is placed

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,10 @@
+class UserMailer < ApplicationMailer
+
+  default from: 'no-reply@jungle.com'
+
+  def order_receipt_email(order)
+    @order = order
+    mail(to: order.email.to_s, subject: 'Your order #{@order.id}')
+  end
+
+end
